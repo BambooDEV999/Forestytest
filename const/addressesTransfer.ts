749af994a93dac32b018229e2123dc1a18a10974
BambooDEV999 <bamboodev9@gmail.com>
2024-04-06 10:53:17 -0400

    SE AGREGA

---
+++ const/addressesTransfer.ts
@@ -0,0 +1,5 @@
+export const TRANSFER_CONTRACT_ADDRESS = '0x7be31F350B59223e768779c36936f64Bb11FCbE7';
+
+
+export const HERO_IMAGE_URL = 'ipfs://QmSE9d5yVa9RD7PgvyFTEAqa9VTRLgpWMDuyWSh8nc1RZT/tranfers%20imagen%201.jpg';
+export const FEATURES_IMAGE_URL = 'ipfs://QmQJXBCi3ZBbhBnViMCNf8ke5GBQLGgqTcPeYgufUuEYnf/tranfers%20web%202.jpg';
